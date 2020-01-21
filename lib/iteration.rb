6ecfeda2cfce577079ac964bc617deb35bb42eb2
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-21 20:34:19 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -40,6 +40,8 @@ def total_even_pairs(src)
   total = 0
   row_index = 0
   while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do
     if src[row_index][0] % 2 == 0 && src[row_index][1] == 0
       total += src[row_index][0] + src[row_index][1]
     end
